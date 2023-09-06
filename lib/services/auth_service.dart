18e2991a976d090e3298ef57a1bd33db8f0e088e
chinnakrit <gukand2001@gmail.com>
2023-09-06 17:13:12 +0700

    first commit

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,61 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  Future<void> signUpWithEmail(String email, String password, String name,
+      String tel, String idline) async {
+    try {
+      // Create New User Account to Firebase Authen
+      final credential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      final uid = credential.user!.uid;
+      print(uid);
+
+      // Store User infomation (name, tel) to FireStore
+      FirebaseFirestore.instance.collection("Users").doc(uid).set({
+        "email": email,
+        "password": password,
+        "name": name,
+        "tel": tel,
+        "idline": idline,
+      });
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  static Future<int> loginUser(String email, String password) async {
+    try {
+      final Credential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      print("Successful");
+      return 1;
+    } on FirebaseAuthException catch (e) {
+      print("Failed");
+      print(e.code);
+
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+      return 2;
+    } catch (e) {
+      print(e);
+    }
+    return 0;
+  }
+
+  Future<void> signOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
+}
---
+++ lib/views/loginpage.dart
@@ -0,0 +1,125 @@
+import 'package:finalproject/services/auth_service.dart';
+import 'package:finalproject/views/homepage.dart';
+import 'package:finalproject/views/registerpage.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+final _email = TextEditingController();
+final _password = TextEditingController();
+final _formKey = GlobalKey<FormState>();
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            color: Colors.blue[900],
+            child: Form(
+              key: _formKey,
+              child: Center(
+                child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    margin: EdgeInsets.all(32),
+                    padding: EdgeInsets.all(24),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        SizedBox(
+                          width: 320,
+                          child: Column(
+                            children: [
+                              buildEmailInput(),
+                              buildPasswordInput(),
+                            ],
+                          ),
+                        ),
+                        buildEmailSignin(),
+                        buildEmailSignup(),
+                      ],
+                    )),
+              ),
+            )));
+  }
+
+  Widget buildEmailSignup() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Text("Don't have an account?"),
+          TextButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const RegisterPage(),
+                  ));
+            },
+            child: const Text("Register now !"),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildEmailSignin() {
+    return ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStatePropertyAll(Colors.blue[900]),
+          foregroundColor: const MaterialStatePropertyAll(Colors.white),
+        ),
+        onPressed: () {
+          if (_formKey.currentState!.validate()) {
+            AuthService.loginUser(_email.text, _password.text).then((value) {
+              if (value == 1) {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Homepage()),
+                );
+              }
+            });
+          }
+        },
+        child: const Text("Sing in"));
+  }
+
+  TextFormField buildEmailInput() {
+    return TextFormField(
+      controller: _email,
+      decoration: const InputDecoration(labelText: "E-mail"),
+      validator: (value) {
+        if (value!.isEmpty) {
+          return "กรุณากรอก Email";
+        }
+        return null;
+      },
+    );
+  }
+
+  TextFormField buildPasswordInput() {
+    return TextFormField(
+      controller: _password,
+      obscureText: true,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return "กรุณากรอก Password";
+        }
+        return null;
+      },
+      decoration: const InputDecoration(
+        labelText: "Password",
+      ),
+    );
+  }
+}
